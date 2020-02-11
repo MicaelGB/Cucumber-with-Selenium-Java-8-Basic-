d4cdd6bae1adb8944570a069e9c944a7941b183e
MicaelGB <up160561@alumnos.upa.edu.mx>
2020-02-11 16:57:10 -0600

    Sección 2, abre browser, ingresa con username y password, muestra página principal

--- CucumberBasics/src/test/java/Steps/Hook.java
+++ CucumberBasics/src/test/java/Steps/Hook.java
@@ -3,7 +3,12 @@ package Steps;
 import Base.BaseUtil;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.sql.Driver;
 
 public class Hook extends BaseUtil {
 
@@ -15,13 +20,15 @@ public class Hook extends BaseUtil {
 
     @Before
     public void InitializeTest(){
-        System.out.println("Opening the browser : Firefox");
+        System.out.println("Opening the browser : Chrome");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\llh\\chromedriver.exe");
+        base.Driver = new ChromeDriver();
+        //WebDriver driver = new FirefoxDriver();
 
-        System.setProperty("webdriver.firefox.marionette", "D:\\libs\\geckodriver.exe");
-        Driver = new FirefoxDriver();
     }
     @After
     public void TearDownTest(){
         System.out.println("Closing the browser : MOCK");
+        base.Driver.quit();
     }
 }
--- CucumberBasics/src/test/java/Steps/LoginSteps.java
+++ CucumberBasics/src/test/java/Steps/LoginSteps.java
@@ -7,6 +7,8 @@ import cucumber.api.Transform;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import sun.security.util.Password;
 
 import java.lang.reflect.Constructor;
@@ -22,7 +24,7 @@ public class LoginSteps extends BaseUtil {
     }
 
     @Given("^I navigate to the login page$")
-    public void iNavigateToTheLoginPage() throws Throwable{
+    public void iNavigateToTheLoginPage() {
         System.out.println("I navigate to the login page");
         base.Driver.navigate().to("http://www.executeautomation.com/demosite/Login.html");
     }
@@ -30,19 +32,16 @@ public class LoginSteps extends BaseUtil {
 
     @And("^I click login button$")
     public void iClickLoginButton() {
-        System.out.println("I click login button");
+
+        base.Driver.findElement(By.name("Login")).submit();
     }
 
     @Then("^I should see the userform page$")
     public void iShouldSeeTheUserformPage() {
-        System.out.println("I should see the userform page");
     }
 
     @And("^I enter the following for Login$")
     public void iEnterTheFollowingForLogin(DataTable table) {
-        //List<List<String>> data = table.raw();
-        //System.out.println("The value is: " + data.get(0).get(0).toString());
-        //System.out.println("The value is: " + data.get(0).get(1).toString());
 
         //Create an array list
         List<User> users = new ArrayList<User>();
@@ -50,10 +49,10 @@ public class LoginSteps extends BaseUtil {
         users = table.asList(User.class);
 
         for (User user: users){
-            System.out.println("The username is " + user.username);
-            System.out.println("The password is " + user.password);
-
+            base.Driver.findElement(By.name("UserName")).sendKeys(user.username);
+            base.Driver.findElement(By.name("Password")).sendKeys(user.password);
         }
+
     }
 
     @And("^I enter ([^\"]*) and ([^\"]*)$")
