d4d329bcb44e1c66db671a6925bdf5bbb03ab942
MicaelGB <up160561@alumnos.upa.edu.mx>
2020-02-12 11:02:18 -0600

    Correr test de Cucumber with TestNG

--- CucumberBasics/src/test/java/runner/TestRunner.java
+++ CucumberBasics/src/test/java/runner/TestRunner.java
@@ -2,10 +2,11 @@ package runner;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/java/Features"}, glue = "Steps")
 
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests {
 }
